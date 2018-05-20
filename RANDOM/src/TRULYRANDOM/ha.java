2339d884b51229d1e323b1c54f71fb6e4d9edd4d
Justin Kim <juskim88@gmail.com>
2018-05-20 09:53:16 -0700

    New Commit

--- RANDOM/src/TRULYRANDOM/ha.java
+++ RANDOM/src/TRULYRANDOM/ha.java
@@ -1,32 +1,39 @@
 package TRULYRANDOM;
 
-import java.io.*;          // put tester in file into usaco not bin/src the
-					          // folder with bin/src in it
-import java.util.*;
+// put tester in file into usaco not bin/src the
+import java.io.IOException;
+import java.util.HashMap;
 
 public class ha
 {
 	public static void main(String[] args) throws IOException
 	{
-		BufferedReader f = new BufferedReader(new FileReader("a.txt"));
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("words3.txt")));
-		StringTokenizer st = new StringTokenizer(f.readLine());
-		int i=0;
-
-		while (st.hasMoreTokens())
+		int[] a =
+			{ 6, 12, 3, 9, 3, 5, 1 };
+		int k = 12;
+		HashMap<Long, Boolean> usedNum = new HashMap<>();
+		int two=0;
+		for (int i = 0; i < a.length; i++)
 		{
-			out.print("\""+st.nextToken()+"\",");
-			st=new StringTokenizer(f.readLine());
-			i++;
-			if(i==100)
-			{
-				out.print("\n");
-				i=0;
-			}
+			if(a[i]*2==k)
+				two++;
+			usedNum.put((long) a[i], false);
+		}
+		if(two<2)
+			usedNum.remove(k/2);
+
+		int count = 0;
+		for (Long b : usedNum.keySet())
+		{
+			if (!usedNum.get(b))
+				if (usedNum.containsKey(k - b))
+				{
+					count++;
+					usedNum.put(k - b, true);
+					usedNum.put(b, true);
+				}
 		}
 
-		// closing up
-		f.close();
-		out.close();
+		System.out.println(count);
 	}
 }
