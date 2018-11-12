4699805ad9f99f0b04bcbcf239c4faf35cc64268
Justin Kim <juskim88@gmail.com>
2018-11-12 09:28:02 -0800

    New Commit

--- RANDOM/src/usaco/ccski.java
+++ RANDOM/src/usaco/ccski.java
@@ -14,10 +14,29 @@ public class ccski {
 		BufferedReader f = new BufferedReader(new FileReader("ccski.in"));
 		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ccski.out")));
 		StringTokenizer st = new StringTokenizer(f.readLine());
+		int rows = Integer.parseInt(st.nextToken());
+		int cols = Integer.parseInt(st.nextToken());
+		int[][] heights = new int[rows][cols];
+		boolean[][] waypts = new boolean[rows][cols];
+		int numWaypts = 0;
+
+		for (int i = 0; i < rows; i++) {
+			st = new StringTokenizer(f.readLine());
+			for (int j = 0; j < cols; j++) {
+				heights[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for (int i = 0; i < rows; i++) {
+			st = new StringTokenizer(f.readLine());
+			for (int j = 0; j < cols; j++) {
+				waypts[i][j] = Integer.parseInt(st.nextToken()) == 1;
+				numWaypts += waypts[i][j] ? 1 : 0;
+			}
+		}
 		
 		
 
-		out.println(i1 + i2);
+		out.println();
 		out.close();
 		f.close();
 	}
